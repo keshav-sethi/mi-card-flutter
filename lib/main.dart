8eb9953869b3188170096281f6e51fa62c6379b9
keshav-sethi <ksethi_be17@thapar.edu>
2020-06-05 21:08:38 +0530

    something nice for your eyes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,141 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp(),
   );
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: ExactAssetImage('images/profile.jpg'),
+              ),
+              Text(
+                'Begam Khushboo',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'MAKE UP ARTIST',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 20.0,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+91 884 769 2293',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'begumk@gmail.com',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontFamily: 'SourceSansPro',
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+//Row(
+//children: <Widget>[
+//Icon(
+//Icons.email,
+//color: Colors.teal,
+//),
+//SizedBox(
+//width: 20.0,
+//),
+//Text(
+//'begumk@gmail.com',
+//style: TextStyle(
+//fontSize: 20.0,
+//fontFamily: 'SourceSansPro',
+//),
+//),
+//],
+//),
+//
+//Row(
+//children: <Widget>[
+//Icon(
+//Icons.phone,
+//color: Colors.teal,
+//),
+//SizedBox(
+//width: 20.0,
+//),
+//Text(
+//'+91 964 640 2227',
+//style: TextStyle(
+//color: Colors.teal.shade900,
+//fontFamily: 'SourceSansPro',
+//fontSize: 20.0,
+//),
+//),
+//],
+//),
